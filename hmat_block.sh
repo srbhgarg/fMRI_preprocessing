fc3ba3e05fb9796e243f89995ccca6f3fadf5c09
Ryan315 <tian.yori90@gmail.com>
2020-05-22 11:32:09 -0700

    made cahnges related to freesurfer setup and lddmm registration

--- hmat_block.sh
+++ hmat_block.sh
@@ -33,7 +33,7 @@ if ( ! -f "$1/$2/$OUTFILE" ) then
     exit 1
 endif
 
-set file=$OUTFILE
+set T1_file=$OUTFILE
 
 
 set OUTFILE='HMAT_in_struct.nii'
@@ -49,24 +49,27 @@ os.chdir('/home/lab-x399-2/Documents/ndreg/') \
 import ndreg\
 from ndreg import preprocessor, util, plotter \
 import SimpleITK as sitk \
+import os \
 \
 # register colin(in) to bet_t1_file (ref) - get affine and fields \
 # apply affine to HMAT.nii and field to  bet\
 # move the registered image to fMRI space using struct2func.mat \
 \
-colin = util.imgRead('/home/lab-x399-2/Documents/fMRI_preprocessing/colin27_t1_tal_lin_cropped.nii') \
-atlas = util.imgRead('/home/lab-x399-2/Documents/fMRI_preprocessing/HMAT.nii') \
+colin = util.imgRead('/home/lab-x399-2/Documents/fMRI_preprocessing/colin27_t1_tal_lin_cropped2.nii') \
 img = util.imgRead('$output_dir/bet_$T1_file') \
 \
 img_p = sitk.Normalize(img) \
 colin_p = sitk.Normalize(colin) \
-atlas_registered, field, final_transform = ndreg.register_brain(colin_p, img_p, outdir='$output_dir/lddmm') \
+atlas_registered, field, final_transform = ndreg.register_brain(colin_p, img_p, outdir='$output_dir/LDDMM/') \
 \
-field_up = preprocessor.imgResample(field, atlas.GetSpacing()) \
-atlas_affine_up = ndreg.imgApplyAffine(atlas, final_transform) \
+field_up = preprocessor.imgResample(field, colin.GetSpacing()) \
+#atlas = util.imgRead('/home/lab-x399-2/Documents/fMRI_preprocessing/HMAT2.nii') \
+atlas = util.imgRead('/home/lab-x399-2/Documents/fMRI_preprocessing/HMAT2.nii') \
+atlas_affine_up = ndreg.imgApplyAffine(atlas, final_transform, size=img_p.GetSize()) \
 atlas_lddmm_up = ndreg.imgApplyField(atlas_affine_up, field_up) \
 \
-util.imgWrite(atlas_affine_up, '$output_dir/HMAT_in_struct.nii' )\
+util.imgWrite(atlas_affine_up, '$output_dir/HMAT_in_affine_struct.nii' )\
+util.imgWrite(atlas_lddmm_up, '$output_dir/HMAT_in_struct.nii' )\
 os.chdir(cwd) \" > $output_dir/lddmm.py
 
 
